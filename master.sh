4659e50cd0a2e6a7c3ff38c32600c06e539f6b6d
dkhutch <david.hutchinson@unsw.edu.au>
2026-09-17 14:15:58 +1000

    begin master script

---
+++ master.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# 1. Interp topography to ocean grid
+
+# 2. Adjust for narrow straits and isolated grid cells
+
+# 3. Make coupler exchange grids
+
+# 4. Make atmosphere topography
+
+# 5. Create input file for mountain drag parameterisation
+
+# 6. Create river runoff
+
+# 7. Make Vegetation from DeepMIP input files
+
+# 8. Make soil type and groundwater field
+
+# 9. Set greenhouse gases
+
+# 10. Set aerosol forcing
+
+# 11. Make tidal forcing fields (bottom roughness)
+
+# 12. Make temperature-salinity restart file for the ocean
